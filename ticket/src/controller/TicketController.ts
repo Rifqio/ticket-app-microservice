71e72a88bfa32cd5f047f95155c52d22ec622663
Rifqi Oktario <oktariorifqi@gmail.com>
2023-12-31 23:45:52 +0700

    #13 Add get single ticket route

--- ticket/src/controller/TicketController.ts
+++ ticket/src/controller/TicketController.ts
@@ -1,7 +1,9 @@
-import { Logger } from '@rifqioktario/ticketing-common';
+import { BaseResponse, Logger } from '@rifqioktario/ticketing-common';
 import { Request, Response } from 'express';
-import { from, map, of, switchMap } from 'rxjs';
+import { from, map, switchMap, tap } from 'rxjs';
 import { Ticket } from '../models';
+import { isEmpty } from 'lodash';
+import mongoose from 'mongoose';
 
 const Namespace = 'TicketController';
 
@@ -9,20 +11,54 @@ export class TicketController {
     static async createTicket(req: Request, res: Response) {
         const { title, price } = req.body;
 
-        const newTicket = new Ticket({
+        const newTicket$ = new Ticket({
             title,
             price,
             userId: req.currentUser?.id!,
         });
 
-        from(newTicket.save()).subscribe({
+        from(newTicket$.save())
+            .pipe(
+                map((ticket) => {
+                    return {
+                        id: ticket.id,
+                        title: ticket.title,
+                        price: ticket.price,
+                    };
+                }),
+            )
+            .subscribe({
+                next: (ticket) => {
+                    Logger.info(`[${Namespace}, createTicket] Ticket created successfully: ${ticket.id}`);
+                    return res
+                        .status(201)
+                        .json(BaseResponse.successSaveResponse(ticket));
+                },
+                error: (error) => {
+                    Logger.error(
+                        `[${Namespace}, createTicket] Error: ${error.message} | Stack: ${error.stack}`,
+                    );
+                    return res.boom.badImplementation();
+                },
+            });
+    }
+
+    static async getTicket(req: Request, res: Response) {
+        const { id } = req.params;
+        
+        from(Ticket.findById(id)).subscribe({
             next: (ticket) => {
-                Logger.info(`[${Namespace}, createTicket] Ticket created successfully: ${ticket.id}`);
-                return res.json({ data: ticket });
+                if (isEmpty(ticket)) {
+                    Logger.info(`[${Namespace}, getTicket] Ticket not found`);
+                    return res.boom.notFound('Ticket not found');
+                }
+
+                Logger.info(`[${Namespace}, getTicket] Ticket found successfully: ${ticket.id}`);
+                return res.json(BaseResponse.successResponse({ data: ticket, message: 'Ticket Found' }));
             },
             error: (error) => {
-                Logger.error(`[${Namespace}, createTicket] Error: ${error.message}`);
-                return res.boom.badRequest(error.message);
+                Logger.error(`[${Namespace}, getTicket] Error: ${error.message} | Stack: ${error.stack}`);
+                return res.boom.badImplementation();
             },
         });
     }
