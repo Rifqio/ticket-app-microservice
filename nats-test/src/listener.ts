d02ac53da6708795523947cbb94bde4e54e33404
Rifqi Oktario <oktariorifqi@gmail.com>
2024-01-06 18:58:38 +0700

    #19 Configuring nats to use durable queue

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -8,21 +8,28 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.clear();
     console.log('LISTENER - Listener connected to NATS');
-    
+
     stan.on('close', () => {
         console.log('LISTENER - NATS connection closed');
         process.exit();
     });
-    
-    const options = stan.subscriptionOptions().setManualAckMode(true);
+
+    const options = stan
+        .subscriptionOptions()
+        .setManualAckMode(true)
+        .setDeliverAllAvailable()
+        .setDurableName('ticketing-service');
+
     const subscription = stan.subscribe(
         'Ticket::Created',
-        'ticket-service-queue-group',
+        'ticket-queue-group',
         options
     );
 
     subscription.on('message', (event: Message) => {
-        console.log(`LISTENER - Message received | ID: ${event.getSequence()} | ${event.getSubject()} | Data: ${event.getData()}`);
+        console.log(
+            `LISTENER - Message received | ID: ${event.getSequence()} | ${event.getSubject()} | Data: ${event.getData()}`
+        );
         event.ack();
     });
 });
